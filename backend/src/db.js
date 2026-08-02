bbcff56a984c26b8a79c0e36c98a1a476c733fdb
Benjamin <jorgeruizbenjamin@gmail.com>
2026-08-01 20:08:20 -0400

    Soporte SSL para conexiones externas a PostgreSQL

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,12 +3,15 @@ import "dotenv/config";
 
 const { Pool } = pg;
 
+const useSSL = process.env.PGSSLMODE === "require";
+
 export const pool = new Pool({
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT || 5432),
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("error", (err) => {
